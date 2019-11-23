34e66272f1dd2649307565bec1019b9e023e513d
Estérfano Lopes <esterfano.lopes@gmail.com>
2019-11-23 20:34:28 -0300

    Updated csvtojson file and some minor stuffs

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 const nodemailer = require('nodemailer');
 const template = require('./templates/template.js');
-const csv = require('csvtojson/v1');
+const csv = require('csvtojson');
 const schedule = require('node-schedule');
 
-const testfile = './resources/test_list.csv';
-//my test list
-const prodfile = './resources/list.csv';
-//path to our production list
+const path = require('path');
+const appDir = path.dirname(require.main.filename);
+
+const prodfile = `${appDir}/resources/list.csv`;
 
 const {
   userEmail,
@@ -21,10 +21,10 @@ const account = {
 };
 
 const transporter = nodemailer.createTransport({
-  pool: true, //keeps the server connection open
-  host: emailHost, //your email server
-  port: emailPort, //gmail uses SSL
-  secure: true, //gmail sends secure
+  pool: true,
+  host: emailHost,
+  port: emailPort,
+  secure: true,
   auth: {
     user: account.user,
     pass: account.pass
@@ -32,22 +32,13 @@ const transporter = nodemailer.createTransport({
 });
 
 let sendlist = [];
-// empty array where we'll keep 
-//all our contacts
-let message_increment = 0;
-//constiable to move to the next 
-//contact
 
 const trigger_sending = (env) => {
-  //env passes our email and name to 
-  //customize the message
-  console.log(env);
   const emailbody = template.generate(env.first).toString();
-  //generates a string to send 
-  //the personalized HTML
+
   transporter.sendMail({
     from: 'Devin from At The Door <devin@atthedoor.app>',
-    to: env.email, //email address of our recipient
+    to: env,
     subject: 'Events and first impressions At The Door ',
     text: '##Plaintext version of the message##',
     html: emailbody,
@@ -62,25 +53,29 @@ const trigger_sending = (env) => {
 
 const set_message_delays = () => {
   const message_job = schedule.scheduleJob('*/10 * * * * *', () => {
-    console.log(sendlist);
+    let message_increment = 0;
+
     trigger_sending(sendlist[message_increment]);
     if (message_increment < sendlist.length) {
       message_increment++;
-      // if our increment is less than our list length, 
-      // we'll keep sending
     }
     if (message_increment >= sendlist.length) {
       message_job.cancel();
-      // stop our function when last message is sent
     }
   });
 }
 
 const getList = async () => {
-  const jsonCsv = await csv().fromFile(prodfile)
-  console.log(jsonCsv);
-  sendlist.push(jsonCsv);
-  set_message_delays();
+  try {
+    const jsonCsv = await csv({
+      noheader: true,
+      output: "csv"
+    }).fromFile(prodfile)
+    sendlist.push(jsonCsv);
+    set_message_delays();
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const startApp = () => {
@@ -90,7 +85,6 @@ const startApp = () => {
     } else {
       console.log('Server is ready to take our messages');
       await getList();
-      // trigger the whole app once the mail server is ready
     }
   });
 };
